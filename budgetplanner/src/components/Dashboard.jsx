17073f9a5956feb19c2bb0c08efb6cd8591ee921
Emre Akurek <emreyeprem@gmail.com>
2019-01-30 14:08:41 -0600

    budgetchart

---
+++ budgetplanner/src/components/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../css/output.css'
+
+class Dashboard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+         
+        
+        }
+      }
+
+
+render() {
+
+    return (
+          <div className="body">Hello World!</div>
+    )
+  }
+}
+export default Dashboard;
--- budgetplanner/src/index.js
+++ budgetplanner/src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+//import { createStore } from 'redux'
+// import reducer from './store/reducer'
+import { Provider } from 'react-redux'
+// import history from './history'
+import App from './App'
+import Dashboard from './components/Dashboard'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//let store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+ReactDOM.render(
+
+
+
+
+<BrowserRouter >
+ <Switch>
+   <Route exact path="/" component={Dashboard} />
+
+
+ </Switch>
+
+</BrowserRouter>
+
+
+   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
